0c34454f37cc0245932c2a3900ec0253620beb47
Tabish Raza <ts764raza@gmail.com>
2024-11-18 22:50:48 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Weather from './Weather'
+const App = () => {
+  return (
+    <div className='min-h-screen flex justify-center items-center  bg-lime-300'>
+      <Weather/>
+    </div>
+  )
+}
+
+export default App
